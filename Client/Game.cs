f897141516f7d0935453daa8b642032fa520a2f0
Blezzing <mmhmaster@hotmail.com>
2015-07-15 06:16:33 +0200

    :cake: Simpel texture virker på en quad!

--- Client/Game.cs
+++ Client/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
@@ -9,11 +10,14 @@ namespace Client
 {
     class Game : GameWindow
     {
+        Texture2D texture;
+
         /// <summary>
         /// Constructor
         /// </summary>
         public Game()
         {
+            GL.Enable(EnableCap.Texture2D);
             this.Run(60);
         }
         
@@ -24,6 +28,8 @@ namespace Client
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
+
+            texture = GraphicsTools.LoadTexture("rpgTile040.png");
         }
 
         /// <summary>
@@ -45,7 +51,22 @@ namespace Client
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            //Draw stuff
+            GL.BindTexture(TextureTarget.Texture2D, texture.ID);
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(-0.1f, -0.1f);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(+0.1f, -0.1f);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(+0.1f, +0.1f);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(-0.1f, +0.1f);
+            
+            GL.End();
 
             this.SwapBuffers();
         }
